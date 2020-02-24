362921bbe08f84ade34461feef496eb652eb1f04
Samuele FAVAZZA <sfavazza.github@gmail.com>
2020-02-25 00:30:30 +0100

    Project raw draft

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# transcript
-Emacs Mode For Handy Log File Analysis
+# Transcript Mode #
+
+Emacs mode for handy log file analysis. It is useful to inspect usually text based tools output log files.
+This mode offers line highlighting of the most relevant lines like: `fatal`, `errors`, `warnings`, `critical
+warnings`, `note`.
+
+Basic Configuration
+-------------------
+
+To use this mode ensure that the `*.el` files are in your load path.
+As there is no unified way for tools to generate their output logs, the user should manually define
+the reg-exp for the file naming/extension on which enable this mode.
+For the same reason also the whole set of reg-exp for the lines of interest is required
+
+```lisp
+;; add *.el files location to your load paths
+(add-to-list 'load-path "<path_to_Transcript_mode>/transcript")
+
+(use-package transcript)
+```
+
+Features
+--------
+
+The tool logs analysis gets easy and time-saving with this mode, which features:
+
+- Line of interest highlighting: the faces and regexp of the line of interests are fully
+  customizable.
+- Fast log section navigation: single-key navigation allows to quickly move around the line of
+  interest.
+- Automatically revert the visited log file.
+- The buffer is opened in read-mode to ensure any accidental modification from the user.
+- Extensible line of interest classes.
+
+### Highlighting Options ###
+
+    todo
+    
+### Log File Navigation ###
+
+    todo
+    
+### Line of Interest Definition ###
+
+As the no tools out there agree on a common standard to report the process of their tasks, the user
+might need to define a `profile` according to the specific tool generating a log output.
+
+    todo: show some example about how to define them.
+    It should be a set of definition to run during the package initialization:
+    
+```lisp
+(use-package transcript
+    :init
+    ;; profile for menthor modelsim
+    
+    ;; profile for intel quartus
+    )
+```
+
---
+++ transcript.el
@@ -0,0 +1,79 @@
+;;; transcript.el --- Emacs Mode For Handy Log File Analysis -*- lexical-binding: t -*-
+
+;; Copyright (C) Samuele Favazza
+
+;; Author: Samuele Favazza <sfavazza.github@gmail.com>
+;; URL: https://github.com/sfavazza/transcript
+;; Version: 0.0
+;; Keywords: log, transcript
+;; Package-Requires: 
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; An Emacs Mode To Ease Tools Output Log Files Analysis
+
+;; transcript mode provides line highlighting for lines which requires user attention like: fatal,
+;; errors, warnings, file names and line numbers.
+;; It offers to jump to the reported offending line.
+
+;; For more information, read the transcript-mode documentation on the official repository page:
+
+;; https://github.com/sfavazza/transcript
+
+;;; Code:
+
+;; requirements
+(require 'hi-lock)
+
+;; =================================================================================================
+;; user customizations
+(defgroup transcript nil
+  "Transcript mode customization."
+  :group 'convenience
+  :link (url-link "https://github.com/sfavazza/transcript"))
+
+
+;; =================================================================================================
+;; mode-support definitions
+
+;;TODO: definition of profile concept: list of cons (reg-exp . face)
+
+;; =================================================================================================
+;; mode implementation
+(define-derived-mode transcript-mode
+  ;; deriving from the "special" mode, the buffer becomes read-only
+  special-mode "Transcript"
+  "An emacs mode to ease tools output log files analysis."
+  :group 'transcript
+
+  ;; -----------------------------------------------------------------------------------------------
+  ;; body of the derived mode
+
+  ;; disable automatic fontification to let the user decide how what should be highlighted
+
+  ;; define keywords for font-lock mode
+  ;; source: https://www.gnu.org/software/emacs/manual/html_node/elisp/Search_002dbased-Fontification.html#Search_002dbased-Fontification
+  (setq transcript-font-lock-keywords
+        '("error")           ; errors
+        )
+  (setq font-lock-defaults '(("[Ee]rror" . 'hi-red-b)))
+
+  ;; -----------------------------------------------------------------------------------------------
+  ;; define the most common faces
+  
+  )
+
+;;; transcript.el ends here
